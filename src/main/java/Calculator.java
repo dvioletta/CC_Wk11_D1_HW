4fc43c97031e36f5e2f4f3cacb3ac4eb4a4708ac
dvioletta <anita@anitasquires.com>
2019-03-25 15:53:31 +0000

    calculator tests passing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,21 @@
 public class Calculator {
+    private double firstNumber;
+    private double secondNumber;
+
+
+    public Calculator(double firstNumber, double secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public double canAdd() { return this.firstNumber + this.secondNumber; }
+
+    public double canSubtrack() { return this.firstNumber - this.secondNumber; }
+
+    public double canMultiply() { return this.firstNumber * this.secondNumber;}
+
+    public double canDivide() { return this.firstNumber / this.secondNumber; }
+
+
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    private Calculator calculator;
+
+
+@Before
+public void before() {
+    calculator = new Calculator(3, 5);
 }
+
+@Test
+public void canAdd() {
+    assertEquals(8, calculator.canAdd(), 0.1); }
+
+@Test
+    public void canSubtrack(){
+    assertEquals(-2, calculator.canSubtrack(), 0.1);
+}
+
+@Test
+    public void canMultiply(){
+    assertEquals(15, calculator.canMultiply(), 0.1);
+}
+
+@Test
+    public void canDivide(){
+    assertEquals(0.6, calculator.canDivide(), 0.01);
+}
+
+}
+
